43cf58f43a6783a068761708799069b697317112
abhishek <abhishek.911malviya@gmail.com>
2020-10-06 17:09:16 +0530

    153

--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
+from .forms import TweetForm
 import random
 
 def home_view(request, *args, **kwargs):
     return  render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html',context={"form": form})
 
 
 def tweet_list_view(request, *args, **kwargs):
